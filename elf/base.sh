ca5d89a8e2f4fd688cd55cbd3921b470453d432f
zhaixiaojuan@loongson.cn <67671683+zhaixiaojuan@users.noreply.github.com>
2022-04-21 09:47:24 +0800

    Add Loongarch64 test cases (#2)

--- elf/base.sh
+++ elf/base.sh
@@ -6,3 +6,5 @@ aarch64-linux-gnu-gcc -c base.c -o base-aarch64.o
 aarch64-linux-gnu-gcc base.c -o base-aarch64
 mips64el-linux-gnuabi64-gcc -c base.c -o base-mips64el.o
 mips64el-linux-gnuabi64-gcc base.c -o base-mips64el
+loongarch64-unknown-linux-gnu-gcc -c base.c -o base-loongarch64.o
+loongarch64-unknown-linux-gnu-gcc base.c -o base-loongarch64
