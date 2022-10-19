a53164d1ddcfa5935b7e4a3211d6cf15c07b7ed7
Esme Yi <onelirong@gmail.com>
2022-10-19 10:35:27 +0800

    Add 32-bit test files and update the base.sh.

--- xcoff/base.sh
+++ xcoff/base.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-clang base.c --target=powerpc-ibm-aix-xcoff -c -o base.o
-clang base.c --target=powerpc-ibm-aix-xcoff -o base.xcoff
+clang base.c --target=powerpc64-ibm-aix-xcoff -m64 -c -o base.o
+clang base.c --target=powerpc64-ibm-aix-xcoff -m64 -o base.xcoff
+clang base.c --target=powerpc-ibm-aix-xcoff -m32 -c -o base32.o
+clang base.c --target=powerpc-ibm-aix-xcoff -m32 -o base32.xcoff
