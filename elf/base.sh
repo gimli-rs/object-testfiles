69d175f5f974baa332fca9775aad6d4ca9c7e398
Philip Craig <philipjcraig@gmail.com>
2025-06-29 18:21:26 +1000

    elf: add CREL object file

--- elf/base.sh
+++ elf/base.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 gcc -c base.c -o base.o
 gcc base.c -o base -Xlinker --hash-style=both
+objcopy --only-keep-debug base base.debug
 llvm-objcopy-6.0 --strip-sections base base.strip
 x86_64-linux-gnu-gcc-11 -fcf-protection=full -c base.c -o base-x86_64-gnu-properties.o
 aarch64-linux-gnu-gcc -c base.c -o base-aarch64.o
@@ -19,4 +20,4 @@ objcopy --compress-debug-sections=zstd base-debug.o base-debug-zstd.o
 gcc -shared -fPIC -o libbase.so base.c -Wl,--default-symver
 x86_64-linux-gnu-gcc-13 base.c -o base-relr-x86_64 -Wl,-z,pack-relative-relocs
 i686-linux-gnu-gcc-13 base.c -o base-relr-i686 -Wl,-z,pack-relative-relocs
-objcopy --only-keep-debug base base.debug
+clang-21 -c base.c -o base-crel.o -Wa,--crel,--allow-experimental-crel
