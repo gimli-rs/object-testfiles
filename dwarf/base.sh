de9893e3d35412d7f817cf04cedda5ab9f596294
Philip Craig <philipjcraig@gmail.com>
2025-08-13 16:33:59 +1000

    dwarf: file compiled with -gsplit-dwarf

--- dwarf/base.sh
+++ dwarf/base.sh
@@ -3,3 +3,5 @@ g++-13 $CFLAGS base.cpp -o base-gcc-g1 -g1
 g++-13 $CFLAGS base.cpp -o base-gcc-g2 -g2
 clang++-18 $CFLAGS base.cpp -o base-clang-g1 -g1
 clang++-18 $CFLAGS base.cpp -o base-clang-g2 -g2
+g++-13 $CFLAGS base.cpp -o base-gcc-split -g2 -gsplit-dwarf
+llvm-dwp -e base-gcc-split -o base-gcc-split.dwp
