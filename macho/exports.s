76da291ca2985ac1eef681452aee1db7a1f7d2a9
Jez Ng <me@jezng.com>
2025-11-04 02:49:56 -0500

    macho: Add absolute symbol to exports test

--- macho/exports.s
+++ macho/exports.s
@@ -1,5 +1,5 @@
 .text
-.globl	_foo, _foo_resolver
+.globl	_foo, _foo_resolver, _abs
 
 _foo:
     ret
@@ -8,5 +8,7 @@ _foo_resolver:
      //  This causes ld to generate the stub-and-resolver export trie node.
 	.symbol_resolver	_foo
 	ret
+
+_abs = 0xfffff
 
 .subsections_via_symbols
