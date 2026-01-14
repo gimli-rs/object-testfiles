14a8fdbb890656e9dbb8a7e40f65ee44407464de
Philip Craig <philipjcraig@gmail.com>
2026-01-14 15:06:00 +1000

    dwarf: add DWP files with types and macros

--- dwarf/base.sh
+++ dwarf/base.sh
@@ -8,3 +8,8 @@ clang++-18 $CFLAGS base.cpp -o base-clang-macinfo -gdwarf-4 -fdebug-macro
 clang++-18 $CFLAGS base.cpp -o base-clang-macro -gdwarf-5 -fdebug-macro
 g++-13 $CFLAGS base.cpp -o base-gcc-split -g2 -gsplit-dwarf
 llvm-dwp -e base-gcc-split -o base-gcc-split.dwp
+clang++-18 $CFLAGS base.cpp -o base-clang-split-v4 -g2 -gdwarf-4 -gsplit-dwarf -fdebug-macro -fdebug-types-section
+# llvm-dwp doesn't create .debug_macinfo.dwo
+dwp -e base-clang-split-v4 -o base-clang-split-v4.dwp
+clang++-18 $CFLAGS base.cpp -o base-clang-split-v5 -g2 -gdwarf-5 -gsplit-dwarf -fdebug-macro -fdebug-types-section
+llvm-dwp-18 -e base-clang-split-v5 -o base-clang-split-v5.dwp
