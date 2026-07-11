0668c7ea53c6a7ef20e8541b1c92e3df17e6e289
Philip Craig <philipjcraig@gmail.com>
2026-07-11 14:33:37 +1000

    pe: add export.dll fixture (#40)

---
+++ pe/export.c
@@ -0,0 +1,3 @@
+int exported_named(void) { return 1; }
+int exported_ordinal(void) { return 2; }
+int exported_multi(void) { return 3; }
---
+++ pe/export.sh
@@ -0,0 +1,1 @@
+cl export.c /LD /Feexport.dll /link /NOENTRY /NODEFAULTLIB /DEF:export.def kernel32.lib
