e4dba4d280026d087560048aeae087573dffcfca
Philip Craig <philipjcraig@gmail.com>
2026-07-13 15:32:09 +1000

    pe: add forward by ordinal export

--- pe/export.sh
+++ pe/export.sh
@@ -1,1 +1,3 @@
-cl export.c /LD /Feexport.dll /link /NOENTRY /NODEFAULTLIB /DEF:export.def kernel32.lib
+# link.exe can't create a forward-by-ordinal export, so use lld-link.
+clang-22 -c --target=x86_64-pc-windows-msvc export.c -o export.obj
+lld-link-22 export.obj /DLL /NOENTRY /NODEFAULTLIB /DEF:export.def /OUT:export.dll
