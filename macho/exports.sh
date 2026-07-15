fa0e0a7e822dfd25bcbe36b805f388d655250e96
Philip Craig <philipjcraig@gmail.com>
2026-07-15 16:46:46 +1000

    macho: add more exports

--- macho/exports.sh
+++ macho/exports.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-TMP=$(mktemp -d)
-llvm-mc exports.s -filetype=obj -o "$TMP/exports.o"
+clang -c -arch arm64 -mmacosx-version-min=15.0 exports.s -o exports.o
 
-# Generate a dylib with a stub-and-resolver entry as well as re-exported +
-# aliased symbol entry (dyld_stub_binder is defined in libSystem, hence it
-# is getting re-exported by our -alias flag here).
-ld -dylib -arch arm64 -platform_version macos 15.0.0 15.5 \
-    -syslibroot $(xcrun --show-sdk-path) -lSystem \
-    "$TMP/exports.o" -o libexports.dylib \
+# Add a re-exported + aliased symbol entry.
+ld -r -arch arm64 -platform_version macos 15.0.0 15.5 \
+    exports.o -o exports.o \
     -alias dyld_stub_binder my_stub_binder
 
-rm -rf "$TMP"
+ld -dylib -arch arm64 -platform_version macos 15.0.0 15.5 \
+    -syslibroot $(xcrun --show-sdk-path) -lSystem \
+    exports.o -o libexports.dylib
